6e47a6e6109e6ab8613f5ea77e23fb329d23cb94
kamimura <kamimura_5336@yahoo.co.jp>
2019-03-10 09:42:09 +0900

    [remove] ogp-perser

--- ogp-perser/index.js
+++
@@ -1,7 +0,0 @@
-var parser = require("ogp-parser");
-var url = "https://github.com/";
-parser(url, false).then(function(data) {
-	console.log(data);
-}).catch(function(error) {
-	console.error(error);
-});
